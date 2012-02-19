085835aed34eb2be6da0936eced5c5acd3220aaf
Dale Johnson <dale.johnson@gmail.com>
2012-02-19 14:09:43 -0800

    rearrange directory structure

---
+++ bouncer/types.h
@@ -0,0 +1,24 @@
+#ifndef __types_h__
+#define __types_h__
+
+//#define VERBOSE 1
+#define VVERBOSE 0
+//#define PARANOID 1
+//#define VERSION 2.129
+
+#include <stdint.h>
+#include <string>
+
+namespace dj {
+
+  typedef uint32_t device_t;
+  const std::string g_version = std::string(VERSION);
+
+  inline uint32_t fix_color(uint32_t c) {
+    return ((c * 2) & 0xfefefe) | (c & 0xff000000);
+    //return (c & 0x7fffffff);
+  }
+
+}
+
+#endif
